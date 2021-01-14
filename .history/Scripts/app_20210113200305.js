eace6fce822e1000e300bd2e5156a3f8cec27121
Aaron LeBlanc <aaron33aaron@outlook.com>
2021-01-13 20:10:52 -0500

    initial commit

---
+++ .history/Scripts/app_20210113200305.js
@@ -0,0 +1,24 @@
+/* Custom scripts go here */
+
+// IIFE - Immediately Invoked Function Expression
+// AKA: Anonymous Self-Executing Function
+
+(function(){
+
+    // initialization function
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    for (let index = 0; index < 5; index++) {
+        console.log(index);
+        
+    }
+
+
+    // event listener
+    window.addEventListener("load", Start); 
+})();
+
+
---
+++ .history/Scripts/app_20210113200517.js
@@ -0,0 +1,19 @@
+/* Custom scripts go here */
+
+// IIFE - Immediately Invoked Function Expression
+// AKA: Anonymous Self-Executing Function
+
+(function(){
+
+    // initialization function
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+
+    // event listener
+    window.addEventListener("load", Start); 
+})();
+
+
